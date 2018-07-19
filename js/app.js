b7eb1e0bfc3904d51ba79651e01cf6ebfd2a87e0
Matheus Anido <matheus_apc2@hotmail.com>
2018-07-19 02:02:44 -0300

    Mobile Menu

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).foundation()
 $(function() {
     $(window).scroll(function() {
       var winTop = $(window).scrollTop();
-      if (winTop >= 30) {
+      if (winTop >= 200) {
         $("body").addClass("sticky-shrinknav-wrapper");
       } else{
         $("body").removeClass("sticky-shrinknav-wrapper");
