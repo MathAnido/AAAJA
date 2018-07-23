cc9c5cc708b035ff89b1ea69b210a92b845f9286
Matheus Anido <matheus_apc2@hotmail.com>
2018-07-23 15:14:39 -0300

    SVG

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).foundation()
 $(function() {
     $(window).scroll(function() {
       var winTop = $(window).scrollTop();
-      if (winTop >= 200) {
+      if (winTop >= 30) {
         $("body").addClass("sticky-shrinknav-wrapper");
       } else{
         $("body").removeClass("sticky-shrinknav-wrapper");
